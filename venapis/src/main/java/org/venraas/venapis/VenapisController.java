491746f60062414f90669c3a145543df39af9fa7
eric0329 <asdguo@mail2000.com.tw>
2017-12-25 17:20:57 +0800

    [VenapisController] refinement - GsonBuilder, apply local instance instead of global static instance - get code_name from ES cluster - apply slf4j

--- venapis/src/main/java/org/venraas/venapis/VenapisController.java
+++ venapis/src/main/java/org/venraas/venapis/VenapisController.java
@@ -1,7 +1,6 @@
 package org.venraas.venapis;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -10,11 +9,13 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.venraas.venapis.apollo.raas.CompanyManager;
+import org.venraas.venapis.common.Utility;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -24,9 +25,9 @@ import com.google.gson.JsonParser;
 
 @RestController
 public class VenapisController {
-	private static final org.apache.log4j.Logger datalog = Logger.getLogger("DataLog");
-	private Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-	private String hostname = null;
+	
+	private static final Logger DATE_LOGGER = LoggerFactory.getLogger("DataLog");
+	private static final Logger VEN_LOGGER = LoggerFactory.getLogger(VenapisController.class);
 	
 	@RequestMapping("/log")
 	String log(
@@ -35,52 +36,63 @@ public class VenapisController {
 			@RequestHeader(value = "Origin", defaultValue = "n") String origin_host,
 			HttpServletRequest request, HttpServletResponse response) {
 
-		if (client_ip.equals("n"))
-			client_ip = request.getRemoteAddr();
-
 		//-- content-type:application/x-www-form-urlencoded, must be specified in request header 
 		Map<String, String[]> parameters = new HashMap<String, String[]>(request.getParameterMap());
-
-		//-- derive the $code_name according to the given $token
-		String code_name = null; 
-		for (Map.Entry<String, String[]> entry : parameters.entrySet()) {
-			String k = entry.getKey();
-			String[] v = entry.getValue();
-			if (1 <= v.length) {
-				String actPayload = v[0];
-				JsonParser jp = new JsonParser();
-				JsonElement je = jp.parse(actPayload);
-				if (je.isJsonObject()) {
-					JsonObject actObj = je.getAsJsonObject();
-		            String token = actObj.get("token").getAsString();
-		            CompanyManager comMgr = new CompanyManager();
-		    		code_name = comMgr.getCodeName(token);
-		    		break;
-	    		}
-			}
-		}
-		parameters.put("code_name",new String[] {code_name});
-		
-		parameters.put("agent",new String[] { agent});
-		parameters.put("request_method", new String[] {request.getMethod()});
-		parameters.put("api_logtime",new String[] { ZonedDateTime.now().format(DateTimeFormatter.ISO_ZONED_DATE_TIME)});
-		parameters.put("client_ip",new String[] { client_ip});
 		
 		try {
-			if(hostname == null)
-				hostname = InetAddress.getLocalHost().getHostName();
-			parameters.put("api_loghost", new String[] {hostname});
-		} catch (UnknownHostException e) {
-			// ignore
+			if (client_ip.equals("n"))
+				client_ip = request.getRemoteAddr();				
+	
+			//-- derive the $code_name according to the given $token
+			String code_name = null;
+			String token = null;
+			
+			for (Map.Entry<String, String[]> entry : parameters.entrySet()) {
+				//-- e.g. {"pageload":["{\"action\":\"pageload\",\"uid\":null,...}"]}
+				//   k = "pageload"
+				//   v = ["{\"action\":\"pageload\",\"uid\":null,...}"]
+				String k = entry.getKey();
+				String[] v = entry.getValue();
+				if (1 <= v.length) {
+					String actPayload = v[0];
+					JsonParser jp = new JsonParser();
+					JsonElement je = jp.parse(actPayload);
+					if (je.isJsonObject()) {
+						JsonObject actObj = je.getAsJsonObject();
+			            token = actObj.get("token").getAsString();
+			            CompanyManager comMgr = new CompanyManager();
+			    		code_name = comMgr.getCodeName(token);
+			    		break;
+		    		}
+				}
+			}
+			
+			if (null == code_name)
+				VEN_LOGGER.warn("cann't find the code_name according to the token:%s", token);			
+			
+			parameters.put("code_name", new String[] {code_name});			
+			parameters.put("agent",new String[] { agent});
+			parameters.put("request_method", new String[] {request.getMethod()});
+			parameters.put("api_logtime",new String[] { ZonedDateTime.now().format(DateTimeFormatter.ISO_ZONED_DATE_TIME)});
+			parameters.put("client_ip",new String[] { client_ip});			
+						
+			String hostname = InetAddress.getLocalHost().getHostName();
+			parameters.put("api_loghost", new String[] {hostname});			
+		} catch (Exception ex) {
+			VEN_LOGGER.error(ex.getMessage());
+			VEN_LOGGER.error(Utility.stackTrace2string(ex));
 		}
-		datalog.info(gson.toJson(parameters));
+		
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		DATE_LOGGER.info(gson.toJson(parameters));
 
 		response.setHeader("Access-Control-Allow-Origin", origin_host);
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
 		response.setHeader("Content-Type", "text/html;charset=UTF-8");
 
-		/**Important : Always return a empty string with this API. For the case of jsonp GET request, a empty string will be return. 
+		/**
+		 * Important : Always return a empty string with this API. For the case of jsonp GET request, a empty string will be return. 
 		 * If not empty string, it could be an error in front-end.
 		 */
 		return "";
