6c9f600fef18029d5cb960be59a5cb651c518e89
cupid <cupid@cupid-dev.c.venraas-test.internal>
2024-08-21 20:33:23 +0800

    use basic authentication query ES 8.14

--- apollo/src/main/java/org/venraas/venapis/apollo/raas/CompanyClient.java
+++ apollo/src/main/java/org/venraas/venapis/apollo/raas/CompanyClient.java
@@ -8,6 +8,8 @@ import org.venraas.venapis.common.Utility;
 import org.venraas.venapis.context.Config;
 
 import java.net.InetAddress;
+import java.util.Base64;
+
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import com.google.gson.JsonArray;
@@ -35,25 +37,32 @@ public class CompanyClient {
         String codeName = "";
         
         if (null == token || token.isEmpty()) return codeName;        
-
+        
         try {            
 //            Config conf = Config.getInstance();
 //            String hostname_es_ww = conf.getEs_host_westernwall();
 //            if (null == hostname_es_ww) 
 //                hostname_es_ww = InetAddress.getLocalHost().getHostName();
             
-            String hostname_es_ww = "es-node-01.venraas.private";
+            String hostname_es_ww = "es-comp.venraas.private";
 
             //-- query $code_name based on given $token
             //   https://www.elastic.co/guide/en/elasticsearch/reference/1.7/search-uri-request.html#search-uri-request
             //   e.g. qUri = "http://${hostPath}/_search?q=${tokenKV}&sort=update_dt:desc";
-            String hostPath = String.format("http://%s:9200/%s/%s/_search?", hostname_es_ww, VENRAAS_INDEX_NAME, TYPE_NAME);
+            String username = "elastic";
+            String password = System.getenv("ES_COMP_PWD");
+            String auth = username + ":" + password;
+            String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+            String authHeader = "Basic " + encodedAuth;
+            
+            String hostPath = String.format("http://%s:9200/%s_%s/_search?", hostname_es_ww, VENRAAS_INDEX_NAME, TYPE_NAME);
             String tokenKV = String.format("%s:%s", String.join(".", Com_pkgs.companies, Com_pkgs.token), token);            
             String query= String.format("q=%s&sort=%s:desc&size=1", tokenKV, Com_pkgs.update_dt);
             String qUri = hostPath + query;
             
             Content content = Request.Get(qUri)
             		.connectTimeout((int)Constant.TIMEOUT_SEARCH_MILLIS)
+            		.setHeader("Authorization", authHeader)
             		.execute()
             		.returnContent();
             String jsonStr = content.asString();            
